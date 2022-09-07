3ffc7cf4f37dcbec450350cf72936a427dc83970
Marcos Rossi <101893923+gurrucha@users.noreply.github.com>
2022-09-07 15:32:01 -0300

    Avances Home

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import NavigationBar from "./components/NavigationBar";
+import Home from "./pages/Home";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <NavigationBar />
+      <Home />
     </div>
   );
 }
