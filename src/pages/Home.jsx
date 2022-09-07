2e5cd4e7f4407cd50d10db1437910214ffe950b6
Marcos Rossi <101893923+gurrucha@users.noreply.github.com>
2022-09-07 11:33:26 -0300

    Favicon + home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,20 @@
-import { Button, Modal, Form, Row } from "react-bootstrap";
+import { Col, Row, Container } from "react-bootstrap";
 import Tirador4 from "*.jpg";
+import conjunto from "*.jpg";
 
 export default function home() {
   return (
-    <Row className="d-flex">
-      <col className="flex flex-column">
-        <img src={Tirador4} alt="banner" />
-        <h4>En stock</h4>
-        <h2>COLECCIÓN DE GRIFERÍAS</h2>
-      </col>
-    </Row>
+    <Container>
+      <div class="position-relative">
+        <Row className="d-flex flex-column align-items-center">
+          <Col className="col-md-12">
+            <img src={Tirador4} alt="banner" />
+            <h4>En stock</h4>
+            <h2>COLECCIÓN DE GRIFERÍAS</h2>
+            <img src={conjunto} alt="banner" />
+          </Col>
+        </Row>
+      </div>
+    </Container>
   );
 }
