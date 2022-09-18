95c2e6aa7b61f329f7c44c1dac4a5db33bac2f0b
Marcos Rossi <101893923+gurrucha@users.noreply.github.com>
2022-09-18 14:43:29 -0300

    About improvements

--- src/components/AboutThisProject.jsx
+++ src/components/AboutThisProject.jsx
@@ -20,30 +20,30 @@ function About() {
         <img
           src="https://www.gemainteriores.com/wp-content/uploads/2021/05/Coleccion-Negro-Mate.jpg"
           className="banner w-100 my-4 pb-5 mb-5"
-          alt="tiradores de bronce irregulares"
+          alt="tiradores negros, griferia negra y color-palette sobre alfombra"
         />
       </div>
       <div className="container">
         <div className="row">
-          <div className="col mt-5">
-            <h4>Sobre este proyecto</h4>
+          <div className="col mt-3">
+            <h4>SOBRE ESTE PROYECTO</h4>
             <p className="paragraph">
-              Este E-commerce es el proyecto final desarrollado por{" "}
-              <strong>Mercedes Torrendell, Santiago Dehl y Marcos Rossi</strong> en el marco del
-              Bootcamp-Grupo 2207 de Hack Academy, una institución educativa ubicada en Montevideo,
-              Uruguay especializada en programación, que imparte cursos desde el año 2016. El
-              bootcamp es un curso de tiempo completo que capacita a desarrolladores como Full Stack
-              Jr. El objetivo del proyecto final es poner en práctica los conocimientos adquiridos
-              utilizando diversas tecnologías. Durante las tres semanas que duró insumió una carga
-              horaria de circa 180 hrs por integrante. Uno de los primeros desafíos de nuestro
-              proyecto fue el desarrollo de un diagrama de relaciones de entidad cuidadosamente
-              pensado. Aquí definimos las principales relaciones entre las siguientes entidades: ……
+              Este E-commerce es el proyecto final desarrollado por Mercedes Torrendell, Santiago
+              Dehl y Marcos Rossi en el marco del Bootcamp-Grupo 2207 de Hack Academy, una
+              institución educativa especializada en programación, ubicada en Montevideo, Uruguay.
+              que imparte cursos desde el año 2016. El bootcamp es un curso de tiempo completo que
+              capacita a desarrolladores como Full Stack Jr. El objetivo del proyecto final es poner
+              en práctica los conocimientos adquiridos utilizando diversas tecnologías. Durante las
+              tres semanas que duró insumió una carga horaria de circa 180 hrs por integrante. Uno
+              de los primeros desafíos de nuestro proyecto fue el desarrollo de un diagrama de
+              relaciones de entidad cuidadosamente pensado. Aquí definimos las principales
+              relaciones entre las siguientes entidades: ……
             </p>
           </div>
           <div className="col">
             <img
               src="https://www.gemainteriores.com/wp-content/uploads/2021/04/H02-TUR-BZ-1-1.jpg"
-              className="image about-image d-inline-block w-100 my-4"
+              className="image about-image w-100 my-4"
               alt="tiradores de bronce irregulares"
             />
           </div>
@@ -53,10 +53,10 @@ function About() {
             <img
               src={"https://www.gemainteriores.com/wp-content/uploads/2021/04/H02-CUA-BZ-1.jpg"}
               className="image about-image d-inline-block w-100 my-4"
-              alt="tiradores de bronce cilíndricos"
+              alt="tiradores de bronce en cajonera marrón"
             />
           </div>
-          <div className="col mt-5">
+          <div className="col mt-3">
             <h4>Aspectos organizacionales & MER</h4>
             <p className="paragraph">
               Para la maquetación del proyecto se utilizaron las siguientes herramientas:
@@ -74,22 +74,22 @@ function About() {
           </div>
         </div>
         <div className="row">
-          <div className="col mt-5">
+          <div className="col mt-3">
             <h4>Concepción del proyecto e inspiración</h4>
             <p className="paragraph">
               Uno de los primeros desafíos de nuestro proyecto fue el desarrollo de un diagrama de
               relaciones de entidad cuidadosamente pensado. Aquí definimos las principales
               relaciones entre las siguientes entidades: User, Order, Product y Caegory. Inspiramos
-              la plantilla de nuestro sitio web con la de Gema, un sitio web minorista de minorista
-              de accesorios para interiores, cuidadosamente diseñado. Nutrimos nuestra base de datos
-              con imágenes, descripciones, nombres y precios del propio website.
+              la plantilla de nuestro sitio web con la de Gema, un sitio web minorista de accesorios
+              para interiores, cuidadosamente diseñado. Nutrimos nuestra base de datos con imágenes,
+              descripciones, nombres y precios del propio website.
             </p>
           </div>
           <div className="col">
             <img
               src="https://www.gemainteriores.com/wp-content/uploads/2021/04/H01-PIR-18-BZ-3.jpg"
               className="image about-image d-inline-block w-100 my-4"
-              alt="tiradores negros, griferia negra y color-palette sobre alfombra"
+              alt="tirador pequeño de bronce para puerta"
             />
           </div>
         </div>
@@ -98,10 +98,10 @@ function About() {
             <img
               src={"https://www.gemainteriores.com/wp-content/uploads/2021/04/H02-TUR-BZ-5.jpg"}
               className="image about-image d-inline-block w-100 my-4"
-              alt="Llamador de puerta palm en bronce"
+              alt="dos pares de tiradores en bronce sobre fondo crema"
             />
           </div>
-          <div className="col mt-5">
+          <div className="col mt-3">
             <h4>Metodología de trabajo</h4>
             <p className="paragraph">
               Organizamos nuestro trabajo en tres sprints, utilizando Metodología Ágile. Cada sprint
@@ -150,29 +150,35 @@ function About() {
         <div className="col">
           <img src={teams} className="icons8-microsoft-teams-2019 " alt="logo microsoft teams" />
         </div>
-        <div className="row pt-5 mt-5">
-          <div className="col-3 "></div>
-          <div className="col-3 ">
-            <a
-              className="icons8-trello"
-              href="https://trello.com/b/iMKgVZ0d/e-commerce"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <img src={trello} alt="logotipo Trello" />{" "}
-            </a>
-          </div>
-          <div className="col-3">
-            <a
-              className="icons8-figma"
-              href="https://www.figma.com/file/4K0ucBjB7EOI5obNOMZmAm/e-commerce?node-id=0%3A1"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <img src={figma} alt="logotipo Figma" />{" "}
-            </a>
-          </div>
-          <div className="col-3 "></div>
+        <div className="row pt-5 py-5 my-5 mt-5">
+          <>
+            <h2 className="mb-5 py-5 fw-bold">ENLACES A ESQUEMAS DE ORGANIZACIÓN</h2>
+            <div className="col-3 "></div>
+            <div className="col-3 ">
+              <>
+                <h3></h3>
+                <a
+                  className="icons8-trello"
+                  href="https://trello.com/b/iMKgVZ0d/e-commerce"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img src={trello} alt="logotipo Trello" />{" "}
+                </a>
+              </>
+            </div>
+            <div className="col-3">
+              <a
+                className="icons8-figma"
+                href="https://www.figma.com/file/4K0ucBjB7EOI5obNOMZmAm/e-commerce?node-id=0%3A1"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img src={figma} alt="logotipo Figma" />{" "}
+              </a>
+            </div>
+            <div className="col-3"></div>
+          </>
         </div>
       </div>
       <div className="container w-75">
@@ -186,8 +192,8 @@ function About() {
               className="zoom profile-image d-inline-block w-100 my-4"
               alt="Foto de perfil"
             />
-            <p>Mercedes Torrendell</p>
-            <p>Full Stack Developer Jr</p>
+            <p className="mt-5">Mercedes Torrendell</p>
+            <p className="mt-4">Full Stack Developer Jr</p>
             <a
               className="icons8-linkedin"
               href="https://www.linkedin.com/in/mercedes-torrendell-748826236/"
@@ -212,8 +218,8 @@ function About() {
               className="zoom profile-image d-inline-block w-100 my-4"
               alt="Foto de perfil"
             />
-            <p>Santiago Dehl</p>
-            <p>Full Stack Developer Jr</p>
+            <p className="mt-5">Santiago Dehl</p>
+            <p className="mt-4">Full Stack Developer Jr</p>
             <a
               className="icons8-linkedin"
               href="https://www.linkedin.com/in/santiagojdehl/"
@@ -238,8 +244,8 @@ function About() {
               className="zoom profile-image d-inline-block w-100 my-4"
               alt="Foto de perfil"
             />
-            <p>Marcos Rossi</p>
-            <p>Full Stack Developer Jr</p>
+            <p className="mt-5">Marcos Rossi</p>
+            <p className="mt-4">Full Stack Developer Jr</p>
             <row>
               <a
                 className="icons8-linkedin"
